8e9b5133bf67ef76e6aae99f689d7d7cf48d519c
Bantealem <bantem2021@gmail.com>
2022-09-30 16:57:53 +0300

    add style

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1
+        style={{
+          fontSize: "6rem",
+          fontWeight: "600",
+          marginBottom: "2rem",
+          lineHeight: "1em",
+          color: "#ececec",
+          textTransform: "lowercase",
+          textAlign: "center",
+        }}
+      >
+        todos
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
